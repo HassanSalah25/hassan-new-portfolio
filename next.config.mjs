bf73c4004e581bf97a802651eb6912423ea70c75
HassanSalah25 <salemhassan175@gmail.com>
2025-04-04 15:37:18 +0200

    adjust server js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: isProd ? '/hassan-new-portfolio' : '',
+    assetPrefix: isProd ? '/hassan-new-portfolio/' : '',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
